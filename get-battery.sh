504581b93ee73b3908af7251cf2b5ced35839a67
nicolasRuarte <miqueasnruarte@protonmail.com>
2025-10-05 20:57:46 -0300

    Agregando script para mostrar batería en la barra

---
+++ get-battery.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+battery_percent="$(acpi | grep -Eo "[0-9][0-9]?%")"
+
+echo "Batería: $battery_percent"
+
+[ ${BAT%?} -le  5 ] && exit 33
+[ ${BAT%?} -le  20 ] && echo "#FF8000"
+
+exit 0
+

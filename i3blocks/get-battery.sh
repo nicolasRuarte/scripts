b8a50fdb7b09926a4a8aab9d48c35fa3e92d157f
nicolasRuarte <miqueasnruarte@protonmail.com>
2026-01-27 10:46:37 -0300

    feat: Añadiendo notificaciones para batería baja
    No quiero que se repita lo de la otra noche con Fightcade

--- i3blocks/get-battery.sh
+++ i3blocks/get-battery.sh
@@ -1,10 +1,26 @@
 #!/usr/bin/env bash
-battery_percent="$(acpi | grep -Eo "[0-9][0-9]?[0-9]?%")"
+battery_percent="$(acpi | grep -Eo '[0-9][0-9]?[0-9]?%')"
+battery_state="$(acpi | grep -Eo 'Charging')"
+
+if [ $battery_state = "Charging" ]; then
+    echo "Batería: $battery_percent"
+    exit 0
+fi
+
+# battery_percent devuelve el valor con porcentaje (ej: 10%), el //% le quita el símbolo porcentaje
+if [ "${battery_percent//%}" -eq 20 ]; then
+    notify-send -t 5000 "Batería baja" "Cargá la batería, por favor"
+fi
+
+if [ "${battery_percent//%}" -eq 10 ]; then
+    notify-send -t 5000 "Batería baja" "Cargá la batería, por favor"
+fi
+
+if [ "${battery_percent//%}" -eq 5 ]; then
+    notify-send -t 5000 -u critical "Batería baja" "Cargá la batería, por favor"
+fi
 
 echo "Batería: $battery_percent"
-
-[ ${BAT%?} -le  5 ] && exit 33
-[ ${BAT%?} -le  20 ] && echo "#FF8000"
 
 exit 0
 

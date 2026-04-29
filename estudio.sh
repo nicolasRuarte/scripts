51043551c5db6a9f8a4509b8605116234bd259d2
nicolasRuarte <miqueasnruarte@protonmail.com>
2026-04-29 11:56:08 -0300

    Cambios

--- estudio.sh
+++ estudio.sh
@@ -8,20 +8,27 @@ if [ "$materia" = "" ]; then
 fi
 
 if [ "$materia" = "analisis" ]; then
-    i3-msg "workspace 1; exec --no-startup-id ~/binaries/zen/zen-bin --new-tab 'https://famaf.aulavirtual.unc.edu.ar/course/view.php?id=661&section=3#tabs-tree-start'"
+    i3-msg "workspace 1; exec --no-startup-id ~/binaries/zen/zen-bin --new-tab 'https://famaf.aulavirtual.unc.edu.ar/course/view.php?id=656'"
     sleep 5
+    i3-msg "workspace 1; exec --no-startup-id ~/binaries/zen/zen-bin --new-tab 'https://www.geogebra.org/calculator'"
+    sleep 5
+    cd /home/nico/colegio/facu-1ro/analisis-matematico/
 fi
 
 
 if [ "$materia" = "intro" ]; then
     i3-msg "workspace 1; exec --no-startup-id ~/binaries/zen/zen-bin --new-tab 'https://famaf.aulavirtual.unc.edu.ar/course/view.php?id=717'"
     sleep 5
+    cd /home/nico/colegio/facu-1ro/intro-a-algoritmos/
 fi
 
 
 if [ "$materia" = "discreta" ]; then
     i3-msg "workspace 1; exec --no-startup-id ~/binaries/zen/zen-bin --new-tab 'https://famaf.aulavirtual.unc.edu.ar/course/view.php?id=661'"
+    i3-msg "workspace 1; exec --no-startup-id ~/binaries/zen/zen-bin --new-tab 'https://chatgpt.com/c/69e56bc4-e874-83e9-b902-308e0b0224e5'"
     sleep 5
+
+    cd /home/nico/colegio/facu-1ro/discreta/
 fi
 
 i3-msg "workspace 2; exec --no-startup-id /usr/bin/obsidian"
@@ -29,5 +36,3 @@ sleep 5
 
 i3-msg "workspace 4; exec --no-startup-id ~/binaries/youtube-music-3.11.0/youtube-music"
 sleep 5
-
-cd /home/nico/colegio/facu-1ro

a305dc3130865e331a378ad64dbeeb81b8b87172
nicolasRuarte <miqueasnruarte@protonmail.com>
2026-06-06 18:14:53 -0300

    Añadiendo comando para abrir terminal para estudiar análisis

--- estudio.sh
+++ estudio.sh
@@ -12,23 +12,23 @@ if [ "$materia" = "analisis" ]; then
     sleep 5
     i3-msg "workspace 1; exec --no-startup-id ~/binaries/zen/zen-bin --new-tab 'https://www.geogebra.org/calculator'"
     sleep 5
-    cd /home/nico/colegio/facu-1ro/analisis-matematico/
+    gnome-terminal  -- bash -c "cd /home/nico/colegio/facu-1ro/analisis-matematico/; nvim .; exec bash"
 fi
 
 
 if [ "$materia" = "intro" ]; then
     i3-msg "workspace 1; exec --no-startup-id ~/binaries/zen/zen-bin --new-tab 'https://famaf.aulavirtual.unc.edu.ar/course/view.php?id=717'"
     sleep 5
-    cd /home/nico/colegio/facu-1ro/intro-a-algoritmos/
+    gnome-terminal  -- bash -c "cd /home/nico/colegio/facu-1ro/intro-a-algoritmos/; nvim .; exec bash"
 fi
 
 
 if [ "$materia" = "discreta" ]; then
     i3-msg "workspace 1; exec --no-startup-id ~/binaries/zen/zen-bin --new-tab 'https://famaf.aulavirtual.unc.edu.ar/course/view.php?id=661'"
+    sleep 5
     i3-msg "workspace 1; exec --no-startup-id ~/binaries/zen/zen-bin --new-tab 'https://chatgpt.com/c/69e56bc4-e874-83e9-b902-308e0b0224e5'"
     sleep 5
-
-    cd /home/nico/colegio/facu-1ro/discreta/
+    gnome-terminal  -- bash -c "cd /home/nico/colegio/facu-1ro/discreta/; nvim .; exec bash"
 fi
 
 i3-msg "workspace 2; exec --no-startup-id /usr/bin/obsidian"
@@ -36,3 +36,5 @@ sleep 5
 
 i3-msg "workspace 4; exec --no-startup-id ~/binaries/youtube-music-3.11.0/youtube-music"
 sleep 5
+
+exit

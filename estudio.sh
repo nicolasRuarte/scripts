9be1b422bcc78108d9ff1db96fe6996b8b87d24e
nicolasRuarte <miqueasnruarte@protonmail.com>
2026-08-12 16:02:18 -0300

    Agregando scripts para las materias del segundo cuatri

--- estudio.sh
+++ estudio.sh
@@ -1,31 +1,18 @@
 #!/usr/bin/env bash
 
-materia=$1
+MATERIA=$1
 
-echo $materia
 
-if [ "$materia" = "" ]; then
+if [ "$MATERIA" = "" ]; then
     echo "Por favor, especifica una materia"
     exit 0
 fi
 
-if [ "$materia" = "analisis" ]; then
-    i3-msg "workspace 1; exec --no-startup-id ~/binaries/zen/zen-bin --new-tab 'https://famaf.aulavirtual.unc.edu.ar/course/view.php?id=656'"
-    sleep 5
-    i3-msg "workspace 1; exec --no-startup-id ~/binaries/zen/zen-bin --new-tab 'https://www.geogebra.org/calculator'"
-    sleep 5
-    gnome-terminal  -- bash -c "cd /home/nico/colegio/facu-1ro/analisis-matematico/; nvim .; exec bash"
+if [ "$MATERIA" = "analisis" ]; then
+    z analisis-2; nvim .
 fi
 
-
-if [ "$materia" = "intro" ]; then
-    i3-msg "workspace 1; exec --no-startup-id ~/binaries/zen/zen-bin --new-tab 'https://famaf.aulavirtual.unc.edu.ar/course/view.php?id=717'"
-    sleep 5
-    gnome-terminal  -- bash -c "cd /home/nico/colegio/facu-1ro/intro-a-algoritmos/; nvim .; exec bash"
-fi
-
-
-if [ "$materia" = "discreta" ]; then
+if [ "$MATERIA" = "discreta" ]; then
     i3-msg "workspace 1; exec --no-startup-id ~/binaries/zen/zen-bin --new-tab 'https://famaf.aulavirtual.unc.edu.ar/course/view.php?id=661'"
     sleep 5
     i3-msg "workspace 1; exec --no-startup-id ~/binaries/zen/zen-bin --new-tab 'https://chatgpt.com/c/69e56bc4-e874-83e9-b902-308e0b0224e5'"
@@ -33,10 +20,15 @@ if [ "$materia" = "discreta" ]; then
     gnome-terminal  -- bash -c "cd /home/nico/colegio/facu-1ro/discreta/; nvim .; exec bash"
 fi
 
-i3-msg "workspace 2; exec --no-startup-id /usr/bin/obsidian"
-sleep 5
+if [ $MATERIA = "algebra" ] ; then
+    z algebra; nvim .
+fi
 
-i3-msg "workspace 4; exec --no-startup-id ~/binaries/youtube-music-3.11.0/youtube-music"
+if [ $MATERIA = "algo-1" ] ; then
+    z algos-1; nvim .
+fi
+
+i3-msg "workspace 2; exec --no-startup-id /usr/bin/obsidian"
 sleep 5
 
 exit
